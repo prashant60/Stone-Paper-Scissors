28695bca15faa63d9f6bfc33083e21060d20a1ce
Prashant <prashantgoel60@gmail.com>
2020-11-17 14:21:35 +0530

    Visuality Improved

--- Stone_Paper_Scissor.py
+++ Stone_Paper_Scissor.py
@@ -1,5 +1,6 @@
 import random
 import gui
+from tkinter import *
 comp=0
 user=0
 
@@ -9,22 +10,28 @@ def stone():
     options = ["Stone", "Paper", "Scissor"]
     random_item = random.choice(options)
 
+    gui.photo_u=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\stone.png')
+    
     if random_item == "Stone":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\stone.png')
         gui.variable1.set("Tie!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     elif random_item == "Paper":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\paper.png')
         comp+=1
-        gui.variable1.set("Computer Wins!!!")
+        gui.variable1.set("Computer\nWins!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     else:
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\scissor.png')
         user+=1
         gui.variable1.set("You Win!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
+    gui.var_img()
 
 def paper():
     global comp
@@ -32,22 +39,28 @@ def paper():
     options = ["Stone", "Paper", "Scissor"]
     random_item = random.choice(options)
 
+    gui.photo_u=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\paper.png')
+    
     if random_item == "Stone":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\stone.png')
         user += 1
         gui.variable1.set("You Win!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     elif random_item == "Paper":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\paper.png')
         gui.variable1.set("Tie!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     else:
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\scissor.png')
         comp += 1
-        gui.variable1.set("Computer Wins!!!")
+        gui.variable1.set("Computer\nWins!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
+    gui.var_img()
 
 def scissor():
     global comp
@@ -55,19 +68,25 @@ def scissor():
     options = ["Stone", "Paper", "Scissor"]
     random_item = random.choice(options)
 
+    gui.photo_u=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\scissor.png')
+
     if random_item == "Stone":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\stone.png')
         comp += 1
-        gui.variable1.set("Computer Wins!!!")
+        gui.variable1.set("Computer\nWins!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     elif random_item == "Paper":
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\paper.png')
         user += 1
         gui.variable1.set("You Win!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
 
     else:
+        gui.photo_c=PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\scissor.png')
         gui.variable1.set("Tie!!!")
         gui.comp1.set(comp)
         gui.user1.set(user)
+    gui.var_img()
--- gui.py
+++ gui.py
@@ -21,6 +21,17 @@ mylabel.pack()
 mylabel.config(bg='aqua', bd=0, height=2, fg="red", font=("","14", "bold"))
 mylabel.place(y=242)
 
+def var_img():
+    label_c = Label(root, bg='red', bd=8, relief=SUNKEN)
+    label_c.pack()
+    label_c.config(image=photo_c, width=92, height=42)
+    label_c.place(y=191)
+    
+    label_u = Label(root, bg='red', bd=8, relief=SUNKEN)
+    label_u.pack()
+    label_u.config(image=photo_u, width=92, height=42)
+    label_u.place(x=242, y=191)
+
 def play():
     score1 = Label(root, text="COMPUTER: ")
     score1.pack()
@@ -47,37 +58,42 @@ def play():
     score4.config(bg='yellow', bd=0, height=2, width=7, fg="red", font=("", "14", "bold"))
     score4.place(x=265, y=80)
 
-    stone_button = Button(root, bg='blue', bd=8, relief=RAISED, command=stone)
+    stone_button = Button(root, bg='blue', bd=8, relief=RAISED, cursor="hand2", command=stone)
     stone_button.pack()
     stone_button.config(image=photo1,  width=98, height=48)
     stone_button.place(y=126)
 
-    paper_button = Button(root, bg='blue', bd=8, relief=RAISED, command=paper)
+    paper_button = Button(root, bg='blue', bd=8, relief=RAISED, cursor="hand2", command=paper)
     paper_button.pack()
     paper_button.config(image=photo2, width=99, height=48)
     paper_button.place(x=116, y=126)
 
-    scissor_button = Button(root, bg='blue', bd=8, relief=RAISED, command=scissor)
+    scissor_button = Button(root, bg='blue', bd=8, relief=RAISED, cursor="hand2", command=scissor)
     scissor_button.pack()
     scissor_button.config(image=photo3, width=98, height=48)
     scissor_button.place(x=233, y=126)
-
+    
     label2 = Label(root, textvariable=variable1, relief=SUNKEN)
     label2.pack()
-    label2.config(bg='black', bd=6, height=2, width=28, fg="red", font=("", "14", "bold"))
-    label2.place(y=191)
+    label2.config(bg='black', bd=6, height=2, width=10, fg="red", font=("", "14", "bold"))
+    label2.place(x=108, y=191)
+
+    var_img()
 
 photo = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\photo.png')
 photo1 = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\stone.png')
 photo2 = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\paper.png')
 photo3 = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\scissor.png')
 
+photo_u = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\black_u.png')
+photo_c = PhotoImage(file='C:\\Users\\Prashant\\Documents\\GitHub\\Stone-Paper-Scissors\\black_c.png')
+
 game_logo = Label(root, bg="white", bd=0, relief=SUNKEN)
 game_logo.pack()
 game_logo.config(image=photo, width=350, height=100)
 game_logo.place(y=80)
 
-play_button = Button(root, bg="orange", bd=10, relief=SUNKEN, text="PLAY GAME", command=play)
+play_button = Button(root, bg="orange", bd=10, relief=SUNKEN, text="PLAY GAME", cursor="hand2", command=play)
 play_button.pack()
 play_button.config(width=23, font=("","17", "bold"))
 play_button.place(y=180)
